18cf2e021bdc5270f8f8b907830ad7d517ae8118
aksel-vaillant <82941071+aksel-vaillant@users.noreply.github.com>
2022-10-15 19:13:09 -0400

    Simplified comprehension of the main class

---
+++ src/PhysicEngine.cpp
@@ -0,0 +1,123 @@
+#include "PhysicEngine.h"
+#include "physics/core/Particle.h"
+
+/* ---------- Callbacks ---------- */
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	// Shortcuts
+	if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+	//if (key == GLFW_KEY_P && action == GLFW_PRESS) app.uiManager->startSimulation();
+	//if (key == GLFW_KEY_R && action == GLFW_PRESS) app.uiManager->resetSimulation();
+}
+
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+	PhysicEngine& app = PhysicEngine::getInstance();
+
+	app.WINDOW_WIDTH = width;
+	app.WINDOW_HEIGHT = height;
+}
+
+/* ---------- Methods ---------- */
+
+PhysicEngine& PhysicEngine::getInstance() {
+	static PhysicEngine instance;
+	return instance;
+}
+
+void PhysicEngine::run()
+{
+	initLogger();
+	initGLFW();
+	initOpenGL();
+	initUI();
+
+	spdlog::info("Initialization complete !");
+
+	mainLoop();
+
+	cleanUp();
+}
+
+void PhysicEngine::initLogger() {
+	spdlog::set_level(spdlog::level::debug); // Set global log level to debug
+	spdlog::set_pattern("[%^%l%$] %v");
+}
+
+void PhysicEngine::initGLFW()
+{
+	glfwInit();
+	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGL Game Engine", nullptr, nullptr);
+
+	// Set all the required options for GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_VERSION_MAJOR);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_VERSION_MINOR);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, true);
+	glfwSwapInterval(1); // Enable vsync
+
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetWindowSizeCallback(window, framebuffer_size_callback);
+}
+
+void PhysicEngine::initOpenGL()
+{
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		throw std::runtime_error("Failed to initialize GLAD");
+	}
+	
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT, 0, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void PhysicEngine::initUI()
+{
+	uiManager = new UIManager();
+	uiManager->init(window);
+}
+
+void PhysicEngine::mainLoop()
+{
+	// Create a cube/particle/cicle
+	Particle p(Vector3(1.0f, 100.0f, 1.0f));
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// Updates
+		float currentFrame = (float)glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		p.integrate(deltaTime);
+		cout << "Position - " << p.getPosition() << endl;
+
+		// Render
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		uiManager->render();
+
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+}
+
+void PhysicEngine::cleanUp()
+{
+	uiManager->cleanUp();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
---
+++ src/PhysicEngine.h
@@ -0,0 +1,57 @@
+#pragma once
+
+// Basics
+#include <iostream>						// Standard I/O streams
+#include <sstream>
+#include <stdlib.h>						// Conversion, random and dynamic memory management
+#include <stdio.h>						// I/O operations
+using namespace std;
+
+// Graphics
+#include "glad/glad.h"					
+#include <GLFW/glfw3.h>					// Must be included after glad
+
+// UI
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "ui/UIManager.h"
+
+// Logging
+#include "spdlog/spdlog.h"
+
+class PhysicEngine
+{
+	public:		
+		const int GL_VERSION_MAJOR = 3, GL_VERSION_MINOR = 3;
+
+		GLFWwindow* window;
+		UIManager* uiManager;
+
+		uint32_t WINDOW_WIDTH = 1080;
+		uint32_t WINDOW_HEIGHT = 920;
+
+		float deltaTime = 0.0f;					// Time between current frame and last frame
+		float lastFrame = 0.0f;					// Time of last frame
+
+		/* ---------- Methods ---------- */
+
+		static PhysicEngine& getInstance();
+		void run();
+		
+	private:
+		PhysicEngine(void){}					// private constructor necessary to allow only 1 instance
+		PhysicEngine(PhysicEngine const&);		// prevent copies
+		void operator=(PhysicEngine const&);	// prevent assignments
+
+		/* ---------- Methods ---------- */
+		void initLogger();
+		void initGLFW();
+		void initOpenGL();
+		void initUI();
+
+		void mainLoop();
+
+		void cleanUp();
+};
+
+
