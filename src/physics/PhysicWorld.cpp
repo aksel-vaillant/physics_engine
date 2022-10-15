7b167605fef5f9fc0116f8c04044820053fc4956
aksel-vaillant <82941071+aksel-vaillant@users.noreply.github.com>
2022-10-15 19:13:46 -0400

    Added PhysicWorld class

---
+++ src/physics/PhysicWorld.cpp
@@ -0,0 +1,44 @@
+#include "PhysicWorld.h"
+
+
+unsigned PhysicWorld::generateContacts()
+{
+	unsigned limit = maxContacts;
+	ParticleContact* nextContact = contacts;
+
+	ContactGenerators::iterator i;
+	for (i = contactGenerators.begin(); i != contactGenerators.end(); i++) {
+		unsigned used = (*i)->addContact(nextContact, limit);
+		limit -= used;
+		nextContact += used;
+
+		// Check if we run out of contacts
+		if (limit <= 0) break;
+	}
+
+	// Return the number of contacts used
+	return maxContacts - limit;
+}
+
+void PhysicWorld::integrate(real duration)
+{
+	Particles::iterator p;
+	for (p = particles.begin(); p != particles.end(); p++) {
+		(*p)->integrate(duration);
+	}
+}
+
+void PhysicWorld::runPhysics(real duration)
+{
+	registry.updateForces(duration);
+	integrate(duration);
+
+	unsigned int usedContacts = generateContacts();
+
+	if (usedContacts) {
+		//if (calculateIterations)
+		//	resolver.setIterations(usedContacts * 2);
+		
+		resolver.resolveContacts(contacts, usedContacts, duration);
+	}
+}
---
+++ src/physics/PhysicWorld.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <vector>
+
+#include "contact/ParticleContactGenerator.h"
+#include "contact/ParticleContactResolver.h"
+#include "core/Particle.h"
+#include "force/ParticleForceRegistry.h"
+
+
+class PhysicWorld
+{
+public:
+	typedef std::vector<Particle*> Particles;
+	typedef std::vector<ParticleContactGenerator*> ContactGenerators;
+
+	Particles particles;
+	ParticleForceRegistry registry;
+	ParticleContactResolver resolver;
+	ContactGenerators contactGenerators;
+	
+	// Hold the list of contacts
+	ParticleContact* contacts;
+	// Hold the maximum number of contacts
+	unsigned maxContacts;
+
+public:
+	PhysicWorld();
+
+	PhysicWorld(unsigned maxContacts, unsigned iterations = 0);
+	
+	// Initialize the world for a simulation frame
+	void startFrame();
+
+	// Report their contacts and return the number of generated contacts
+	unsigned generateContacts();
+
+	// Integrate all the particles in the world in time
+	void integrate(real duration);
+
+	// Process all the physics for the physic world
+	void runPhysics(real duration);
+};
